b0fb6ec18a1e3f4a0d252e41228456e593543cc4
Evan Powell <evanpowell87@gmail.com>
2024-02-26 19:22:19 -0600

    update nav styles

--- app/components/main-header.tsx
+++ app/components/main-header.tsx
@@ -6,22 +6,30 @@ import Image from 'next/image';
 const MainHeader = () => {
   return (
     <header>
-      <Link href="/">
-        <Image
-          src={logoImg.src}
-          alt="A plate with food on it"
-          width={50}
-          height={50}
-        />
-        Level Up Food
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/meals">Meals</Link>
-            <Link href="/community">Community</Link>
-          </li>
-        </ul>
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Level Up Food</span>
+            <Image
+              className="h-8 w-auto"
+              src={logoImg.src}
+              alt="A plate with food on it"
+              width={50}
+              height={50}
+            />
+          </Link>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <Link href="/meals" className="text-sm font-semibold leading-6">
+            Meals
+          </Link>
+          <Link href="/community" className="text-sm font-semibold leading-6">
+            Community
+          </Link>
+        </div>
       </nav>
     </header>
   );
